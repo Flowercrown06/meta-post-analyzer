e18ba0bc84a72d246ed49071294aee17c6d474a8
Gultaj <gultacquliyeva26@gmail.com>
2026-06-24 00:20:16 +0400

    refactored config

--- src/main/java/com/nracademy/metapostanalyzer/config/ProjectConfig.java
+++ src/main/java/com/nracademy/metapostanalyzer/config/ProjectConfig.java
@@ -10,8 +10,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectConfig {
 
     static {
+
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        if (System.getProperty("DB_URL") == null && System.getenv("DB_URL") != null) {
+            System.setProperty("DB_URL", System.getenv("DB_URL"));
+            System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+            System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+            System.setProperty("META_ACCESS_TOKEN", System.getenv("META_ACCESS_TOKEN"));
+            System.setProperty("META_API_URL", System.getenv("META_API_URL"));
+        }
     }
 
     @Bean
