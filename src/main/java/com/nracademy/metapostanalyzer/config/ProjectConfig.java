55897bd800586a6f2fb56ae083122b486662456b
Gultaj <gultacquliyeva26@gmail.com>
2026-06-24 12:32:20 +0400

    fix: spring datasource config and explicit postgres dialect

--- src/main/java/com/nracademy/metapostanalyzer/config/ProjectConfig.java
+++ src/main/java/com/nracademy/metapostanalyzer/config/ProjectConfig.java
@@ -1,6 +1,5 @@
 package com.nracademy.metapostanalyzer.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +7,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
-
-    static {
-
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
-        if (System.getProperty("DB_URL") == null && System.getenv("DB_URL") != null) {
-            System.setProperty("DB_URL", System.getenv("DB_URL"));
-            System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
-            System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-            System.setProperty("META_ACCESS_TOKEN", System.getenv("META_ACCESS_TOKEN"));
-            System.setProperty("META_API_URL", System.getenv("META_API_URL"));
-        }
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
